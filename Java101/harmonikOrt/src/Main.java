bf06a1ca6ad4462e8f78402d78760ba33c685f53
yusuferens <109536558+yusuferens@users.noreply.github.com>
2023-08-29 23:15:06 +0300

    Harmonik Ortalama Hesaplama

---
+++ Java101/harmonikOrt/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("Dizinin Boyutu");
+        int n = inp.nextInt();
+
+        double[] arr = new double[n];
+
+        System.out.println("Dizi elemanlarını giriniz:");
+        for (int i = 0; i<n; i++){
+            arr[i] = inp.nextDouble();
+        }
+
+        double sum = 0.0;
+        for (int i = 0 ; i < n ; i++){
+            sum+= 1.0 / arr[i];
+        }
+        double harmonicMean = n / sum;
+        System.out.println(harmonicMean);
+    }
+}
